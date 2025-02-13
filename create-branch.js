4a0cdad53a3ca63d9f3554c6e35376d17605e3a5
wandyezj <wandyezj@gmail.com>
2025-02-13 10:24:24 -0800

    Create create-branch.js

---
+++ create-branch.js
@@ -0,0 +1,42 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path")
+
+const parameters = process.argv.slice(2);
+
+if (parameters.length !== 1) {
+    console.log("usage: [branch name]");
+    process.exit(1);
+}
+
+const [branchName] = parameters;
+
+let branchPrefix = "";
+const configPath = path.join(__dirname, "create-branch.config.json");
+if (fs.existsSync(configPath)) {
+    const data = fs.readFileSync(configPath, {encoding:"utf-8"});
+    try {
+        const config = JSON.parse(data);
+        const prefix = config["prefix"];
+        if (typeof prefix === "string") {
+            branchPrefix = prefix;
+        } else {
+            throw "Invalid Prefix"
+        }
+    } catch {
+        console.error(`Invalid config ${configPath}\n${data}`);
+        process.exit(1);
+    }
+}
+
+
+const branch = `${branchPrefix}${branchName}`
+
+const validBranch = /^([a-z]|-|\/|[0-9])*$/
+
+if (!validBranch.test(branch)) {
+    console.error(`Invalid branch name: ${branch}`);
+    process.exit(1);
+}
+
+execSync(`git checkout -b ${branch}`);
