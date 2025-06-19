602e3ef9d73b9aca1acb632ddd17c3d241be77e5
wandyezj <wandyezj@gmail.com>
2025-06-19 14:59:37 -0700

    Update r.cmd

--- r.cmd
+++ r.cmd
@@ -2,6 +2,6 @@
 REM This script changes the current directory to the one specified in the environment variable 'r'.
 if DEFINED r (
     if EXIST "%r%" (
-        cd %r%
+        cd /D %r%
     )
 )
