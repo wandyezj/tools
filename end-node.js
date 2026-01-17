e892f497de712d5fb1bba28408e4e9e88a0526f3
wandyezj <wandyezj@gmail.com>
2026-01-16 23:12:36 -0800

    Update end-node.js

--- end-node.js
+++ end-node.js
@@ -1,3 +1,16 @@
+const os = require("os");
 const { execSync } = require("child_process");
 
-execSync("taskkill /F /IM node.exe", {stdio: 'inherit'});
+const platform = os.platform();
+const isWindows = platform === "win32";
+const isMac = platform === "darwin";
+
+// Change command depending on the platform
+if (isWindows) {
+    execSync("taskkill /F /IM node.exe", { stdio: "inherit" });
+} else if (isMac) {
+    execSync("killall -9 node", { stdio: "inherit" });
+} else {
+    console.error("Unsupported platform:", platform);
+    process.exit(1);
+}
