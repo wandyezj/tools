dd6f343719014ef068fbff9e5ba79111a9d19e20
wandyezj <wandyezj@gmail.com>
2026-02-13 12:03:46 -0800

    Create add-in-log-on.cmd

---
+++ add-in-log-on.cmd
@@ -0,0 +1,3 @@
+:: https://learn.microsoft.com/en-us/office/dev/add-ins/testing/runtime-logging
+call npx office-addin-dev-settings runtime-log --enable %r%\add-in.log
+call npx office-addin-dev-settings runtime-log
