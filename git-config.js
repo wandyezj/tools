e08fb40f1780a88e15a91757a29bc183731c5ae3
wandyezj <wandyezj@gmail.com>
2026-03-14 15:18:29 -0700

    Update git-config.js

--- git-config.js
+++ git-config.js
@@ -1,3 +1,6 @@
 const { execSync } = require("child_process");
 
-execSync("git config --local --add push.autoSetupRemote true", {stdio: 'inherit'});
+execSync("git config --local push.autoSetupRemote true", { stdio: "inherit" });
+
+// Don't track changes to file mode
+execSync("git config --local core.fileMode false", { stdio: "inherit" });
